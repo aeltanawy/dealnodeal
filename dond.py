b53eb5c3115a22d5b0289139e02c60e6e6f4b284
Abeer Eltanawy <abeer.eltanawy@schrodinger.com>
2018-07-22 18:20:38 -0700

    Changed the box DataFrame when choosing a box

--- dond.py
+++ dond.py
@@ -15,14 +15,21 @@ class NewGame:
         self.win = False
         self.deal = False
         self.box_df = self.box_assignment()
-        self.box_to_print = self.box_df.loc[:, self.box_df.columns != 'value']
 
 
     def box_assignment(self):
         box_value= [0.01,0.10,0.50,1.00,5.00,10.00,50.00,100.00,250.00,500.00,750.00,1000.00,3000.00,5000.00,10000.00,15000.00,20000.00,35000.00,50000.00,75000.00,100000.00,250000.00]
         random.shuffle(box_value)
-        box_df = pd.DataFrame({'state': 'closed', 'value': box_value}, index=range(1,23))
+        df = pd.DataFrame({'state': 'closed', 'value': box_value}, index=range(1,23))
+        box_df = df.rename_axis("box", axis="columns")
         return box_df
+
+    def box_update(self, num, value):
+        self.box_df.loc[num, 'state'] = value
+
+    def box_to_print(self):
+        self.box_to_print = self.box_df.loc[:, self.box_df.columns != 'value']
+        return self.box_to_print
 
 
 def main():
@@ -41,7 +48,9 @@ def main():
         print(error_message(between=True, first=1, last=22))
 
     g = NewGame(name, chosen_box)
-    print(g.box_to_print)
+    g.box_update(chosen_box, '--chosen')
+    print(f'\n{g.box_to_print()}')
+    menu()
 
 
 def error_message(between=False, first=0, last=0, type=False, value=0):
@@ -57,5 +66,8 @@ def welcome_msg():
     print('\n|' + '-' * 5 + '-' * (len(game_name) + 2) + '-' * 5 + '|')
     print('|' + "-" * 5 + f' {game_name} ' + '-' * 5 + '|')
     print('|' + '-' * 5 + '-' * (len(game_name) + 2) + '-' * 5 + '|\n')
+
+def menu():
+    print('\nMenu: (1) Play | (2) New | (3) Analysis | (9) Exit\n')
 
 main()
