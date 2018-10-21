d9c9f676a85a13c47f1031651449e6bf7f92c7c4
Abeer Eltanawy <abeer.eltanawy@gmail.com>
2018-10-21 01:13:31 -0700

    Updated dnd_v3.py

--- dond_v3.py
+++ dond_v3.py
@@ -1,5 +1,5 @@
-# import random
-# import enum
+import random
+from enum import Enum
 
 
 class DNDApp:
@@ -40,7 +40,6 @@ class DNDApp:
             new_player = DNDPlayer(new_player_name)
             self.player_list.append(new_player)
 
-
         return self.player_list[player_num-1]
 
     def keep_playing(self):
@@ -49,10 +48,38 @@ class DNDApp:
 
 class DNDGame:
     def play(self):
+
+        # Setup the 22 boxes for the current game.
+        self.boxes = DNDBox.setup_boxes()
+
+        # Choose a box number to keep at the begining of the game.
+        self.choose_player_box()
+
+        # Start of game loop
+        end_game = False
+        while not end_game:
+            self.show_boxes_and_menu()
+
+
         return
+
+    def show_boxes_and_menu(self):
+        # To show boxes dict and game menu.
+        print('\n'.join(f'{k}: {v[1]}' for k, v in self.boxes.items()))
+
+        print('\nMenu:', end=" ")
+        for i in Menu:
+            print('f({i.value}) {i.name}', end=" | ")
+
 
     def endingMoney(self):
         return 100
+
+    def choose_player_box(self):
+        player_box_num = int(input('Choose a box number from 1 to 22 to keep: '))
+        player_box_state = BoxState(2).name
+
+        DNDBox.update_box_state(player_box_num, player_box_state)
 
 
 class DNDPlayer:
@@ -62,6 +89,41 @@ class DNDPlayer:
         self.record = []
         self.email_address = email
 
+class DNDBox:
+
+    def update_box_state(self, num, state):
+        DNDGame.boxes[num][1] = state
+
+    def shuffle_box_values(self):
+        # shuffle values between 0.01 and 25000 randomly in a list and returns that list.
+        values = [0.01,0.10,0.50,1.00,5.00,10.00,50.00,100.00,250.00,50.000,750.00,1000.00,3000.00,5000.00,10000.00,15000.00,20000.00,35000.00,50000.00,75000.00,100000.00,250000.00]
+        box_values = random.shuffle(values)
+
+        return box_values
+
+    def setup_boxes(self):
+        # assign 'closed' state and values to each box. Returns a dict with the boxes value and state.
+        boxes = {}
+        box_state = BoxState(0).name
+        values = self.shuffle_box_values()
+        for index in range(22):
+            box_num = index+1
+            box_value = values[index]
+            boxes[box_num] = [box_value, box_state]
+
+        return boxes
+
+class BoxState(Enum):
+    CLOSED = 0
+    OPENED = 1
+    PLAYER_BOX = 2
+
+
+class Menu(Enum):
+    EXIT = 0
+    PLAY = 1
+    NEW = 2
+    ANALYSIS = 3
 
 
 def load_player_list(): # static method working w/ global data, common for all instances
@@ -74,8 +136,6 @@ def load_player_list(): # static method working w/ global data, common for all i
         p = DNDPlayer(name)
         player_list.append(p)
     return player_list
-
-
 
 
 if __name__ == '__main__':
